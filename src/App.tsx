8a851e11e289255d75800dd622e81817659378e1
Byeolchan Kim <qufstar46@gmail.com>
2024-11-18 17:20:30 +0900

    feat: apply global font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import 'normalize.css';
+import { globalStyle } from './index.styles';
+import { css, Global } from '@emotion/react';
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <div
+        css={css`
+          font-family: 'Pretendard';
+          font-weight: 500;
+        `}
+      >
+        Hello World!
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/index.styles.ts
@@ -0,0 +1,6 @@
+import * as fontStyles from 'pretendard/dist/web/static/pretendard-dynamic-subset.css';
+import { css } from '@emotion/react';
+
+export const globalStyle = css`
+  ${fontStyles}
+`;
