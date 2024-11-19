115a28a27242344967aea2f5e218e2f758a101fe
Byeolchan Kim <qufstar46@gmail.com>
2024-11-19 19:28:57 +0900

    deps: add tanstack router and related plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+
+const isTest = process.env.NODE_ENV === 'test';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), isTest && TanStackRouterVite()],
 });
