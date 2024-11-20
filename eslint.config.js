89f60562eedca58eaddbf369c87ec05135a9c30a
Byeolchan Kim <qufstar46@gmail.com>
2024-11-20 11:45:31 +0900

    ci: let eslint ignore React imports

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^React$',
+        },
+      ],
     },
   }
 );
