faa1f1ea79c5fd4c1c54b7197e9d1b5e7145f721
Byeolchan Kim <qufstar46@gmail.com>
2024-11-21 00:07:38 +0900

    storybook: add skeleton code

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,28 @@
+import Input from '../common/components/Input';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'common/Input',
+  component: Input,
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    value: '동대구',
+    onValueChange: (_) => {
+      return;
+    },
+  },
+};
+
+export const EmptyState: Story = {
+  args: {
+    value: '',
+    onValueChange: (_) => {
+      return;
+    },
+  },
+};
