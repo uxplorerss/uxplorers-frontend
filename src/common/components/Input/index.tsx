700d332cbec59467a8c2b5b92a30796a77707529
Byeolchan Kim <qufstar46@gmail.com>
2024-11-24 00:11:58 +0900

    style: Input component

--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -13,9 +13,11 @@ export default function Input({
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     onValueChange(event.target.value);
   };
+  const theme = useTheme();
 
   return (
     <input
+      css={useInput(theme)}
       type={type}
       value={value}
       onChange={handleChange}
