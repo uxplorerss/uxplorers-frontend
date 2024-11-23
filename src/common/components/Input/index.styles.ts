24cb9d7764163e742878f3a91443be07b4dcd631
Byeolchan Kim <qufstar46@gmail.com>
2024-11-23 22:36:12 +0900

    hotfix: remove styling

--- src/common/components/Input/index.styles.ts
+++ src/common/components/Input/index.styles.ts
@@ -1,17 +1,3 @@
 import { css, Theme } from '@emotion/react';
 
-export const useInput = (theme: Theme) =>
-  css({
-    backgroundColor: theme.colors.gray.lightest,
-    borderWidth: 0,
-    padding: `${theme.spacing.normal} ${theme.spacing.normal}`,
-    fontSize: theme.fontSize.normal,
-    lineHeight: theme.lineHeight.normal,
-    fontWeight: theme.fontWeight.normal,
-    borderRadius: theme.radii.normal,
-    color: theme.colors.gray.blackened,
-    outline: 'none',
-    '::placeholder': {
-      color: theme.colors.gray.base,
-    },
-  });
+export const useInput = (theme: Theme) => css({});
