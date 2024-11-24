374e30a458fbd71bece0dc3028a76020bde6893c
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-11-24 13:12:21 +0000

    style: 임시 Button components 추가

---
+++ src/common/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { container } from './index.styles';
+
+export default function Button({ children, ...rest }: { children: React.ReactNode } & ComponentPropsWithoutRef<'button'>) {
+  return (
+    <button css={container} {...rest}>{ children }</button>
+  );
+}
