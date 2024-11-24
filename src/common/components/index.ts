e67d16e16d3649c2d3435ff87eea1e24fa6949b7
김지환 (Jihwan Kim) <kjhwan0802@naver.com>
2024-11-24 12:28:24 +0000

    feat: import 구문 간소화를 위한 components/index.ts 추가

---
+++ src/common/components/index.ts
@@ -0,0 +1,9 @@
+import BottomBar from './BottomBar';
+import Card from './Card';
+import FontProvider from './FontProvider';
+import Input from './Input';
+import TopBar from './TopBar';
+import Typography from './Typography';
+
+
+export { BottomBar, Card, FontProvider, Input, TopBar, Typography };
