633e6ae62b50f269faa6caed371d8d3e5191e07f
Byeolchan Kim <qufstar46@gmail.com>
2024-11-25 13:56:36 +0900

    refactor: include namespace

--- src/common/components/Button/index.types.ts
+++ src/common/components/Button/index.types.ts
@@ -1,6 +1,4 @@
-import { ReactNode } from '@tanstack/react-router';
-import { ComponentPropsWithoutRef } from 'react';
-
-export interface ButtonPropsType extends ComponentPropsWithoutRef<'button'> {
-  children: ReactNode;
+export interface ButtonPropsType
+  extends React.ComponentPropsWithoutRef<'button'> {
+  children: React.ReactNode;
 }
--- src/common/components/Input/types.ts
+++ src/common/components/Input/types.ts
@@ -1,6 +1,5 @@
-import { ComponentPropsWithoutRef } from 'react';
-
-export interface InputPropsType extends ComponentPropsWithoutRef<'input'> {
+export interface InputPropsType
+  extends React.ComponentPropsWithoutRef<'input'> {
   value: string;
   onValueChange: (value: string) => void;
   error?: string;
