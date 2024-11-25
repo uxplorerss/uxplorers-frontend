23a956dc103917bc4ac9d370afc26181071dafcb
kim jihwan <kjhwan0802@naver.com>
2024-11-25 21:22:24 +0900

    feat: 고속버스 터미널 정보 추가 터미널 100 단위로 지역 구분입니다 지역 구분은 티머니 사이트와 동일합니다

--- src/routes/booking/index.tsx
+++ src/routes/booking/index.tsx
@@ -1,61 +1,31 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Typography, TopBar, Input, BottomBar, Button, IconButton, LogoIcon, LanguageSwitchButton } from '../../common/components';
-import LogoImg from '/ios/32.png?url';
+import { createFileRoute } from '@tanstack/react-router';
+import {
+  Typography,
+  TopBar,
+  Input,
+  BottomBar,
+  Button,
+  IconButton,
+  LogoIcon,
+  LanguageSwitchButton,
+} from '../../common/components';
 import TransferBtn from '../../assets/booking/btn_transfer.svg';
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
 export const Route = createFileRoute('/booking/')({
   component: RouteComponent,
-})
-
-const StyledTopBarLeft = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`
-
-const StyledSelect = styled.select`
-  border-style: none;
-  border-color: transparent;
-  cursor: pointer;
-
-  &:focus {
-    outline: none;
-  }
-`
-
-function TopBarContents({position}:{position: "left" | "right"}) {
-  if (position === 'left') {
-    return (
-      <StyledTopBarLeft>
-        <img src={LogoImg} alt='kumoneygo logo'/>
-        <Typography variant='caption1'>KUmoney GO!</Typography>
-      </StyledTopBarLeft>
-    )
-  } else if (position === 'right') {
-    return (
-      <Typography variant='caption1'>
-        <StyledSelect name='languages'>
-          <option value='ko'>KOR</option>
-          <option value='en'>ENG</option>
-        </StyledSelect>
-      </Typography>
-    )
-  }
-  return (
-    <>
-    </>
-  )
-}
+});
 
 function BookmarkList() {
   return (
-    <div style={{padding: "0 20px"}}>
-      <Typography variant='body1' as='p'>즐겨찾기</Typography>
+    <div style={{ padding: '0 20px' }}>
+      <Typography variant="body1" as="p">
+        즐겨찾기
+      </Typography>
       <Button>서울 경부 → 구미</Button>
     </div>
-  )
+  );
 }
 
 const StyledInputContainer = styled.div`
@@ -73,13 +43,16 @@ const StyledInputContainer = styled.div`
       min-width: 0;
     }
   }
-`
+`;
 
 function RouteComponent() {
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
 
-  const handleChange = (handleName: "origin" | "destination" | "time" | "왕복", e: string ) => {
+  const handleChange = (
+    handleName: 'origin' | 'destination' | 'time' | '왕복',
+    e: string
+  ) => {
     console.log(handleName);
     switch (handleName) {
       case 'origin':
@@ -97,8 +70,7 @@ function RouteComponent() {
     }
   };
 
-  const handleSearchBus = () => {
-  };
+  const handleSearchBus = () => {};
 
   const handleTransfer = () => {
     const temp = origin;
@@ -108,27 +80,62 @@ function RouteComponent() {
 
   return (
     <>
-      <TopBar leftSlot={<Button>
-        <LogoIcon />
-      </Button>} rightSlot={<LanguageSwitchButton language='Korean' />}/>
+      <TopBar
+        leftSlot={
+          <Button>
+            <LogoIcon />
+          </Button>
+        }
+        rightSlot={<LanguageSwitchButton language="Korean" />}
+      />
+      <form
+        style={{
+          padding: '0 20px',
+          justifyItems: 'center',
+          position: 'relative',
+        }}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <Typography variant="title1" as="p">
+          어디로 갈까요?
+        </Typography>
 
-      <form style={{padding: "0 20px", justifyItems: 'center', position: 'relative'}} onSubmit={(e) => e.preventDefault()}>
-        <Typography variant='title1' as='p'>어디로 갈까요?</Typography>
-
-        <IconButton src={TransferBtn} alt='transfer btn' onClick={handleTransfer} style={{position: 'absolute', right: '10%', top: '33%'}}/>
+        <IconButton
+          src={TransferBtn}
+          alt="transfer btn"
+          onClick={handleTransfer}
+          style={{ position: 'absolute', right: '10%', top: '33%' }}
+        />
         <StyledInputContainer>
-          <Input value={origin} onValueChange={(e) => handleChange('origin', e)} placeholder='출발지 선택' />
-          <Input value={destination} onValueChange={(e) => handleChange('destination', e)} placeholder='도착지 선택' />
+          <Input
+            value={origin}
+            onValueChange={(e) => handleChange('origin', e)}
+            placeholder="출발지 선택"
+          />
+          <Input
+            value={destination}
+            onValueChange={(e) => handleChange('destination', e)}
+            placeholder="도착지 선택"
+          />
 
-          <div className='date_type'>
-            <Input value={(new Date()).toDateString()} onValueChange={(e) => handleChange('time', e)} placeholder='날짜 선택' />
-            <Input value='' onValueChange={(e) => handleChange('왕복', e)} placeholder='+ 왕복 선택' />
+          <div className="date_type">
+            <Input
+              value={new Date().toDateString()}
+              onValueChange={(e) => handleChange('time', e)}
+              placeholder="날짜 선택"
+            />
+            <Input
+              value=""
+              onValueChange={(e) => handleChange('왕복', e)}
+              placeholder="+ 왕복 날짜 선택"
+            />
           </div>
         </StyledInputContainer>
 
-        <Button onClick={handleSearchBus} style={{margin: "30px 0"}}>조회하기</Button>
+        <Button onClick={handleSearchBus} style={{ margin: '30px 0' }}>
+          조회하기
+        </Button>
       </form>
-
       <BookmarkList />
       <BottomBar />
     </>
