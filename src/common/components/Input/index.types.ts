e76dafa40cbb1775dd76144db7864ee4a4406b66
Byeolchan Kim <qufstar46@gmail.com>
2024-11-25 18:00:38 +0900

    chore: add types

--- src/common/components/Input/index.types.ts
+++ src/common/components/Input/index.types.ts
@@ -1,6 +1,9 @@
+import { SerializedStyles } from '@emotion/react';
+
 export interface InputPropsType
   extends React.ComponentPropsWithoutRef<'input'> {
   value: string;
   onValueChange: (value: string) => void;
   error?: string;
+  css?: SerializedStyles;
 }
