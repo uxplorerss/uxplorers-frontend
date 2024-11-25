d66b4fb0201a125024dfdaecb6c547192d797acb
Byeolchan Kim <qufstar46@gmail.com>
2024-11-25 12:43:28 +0900

    feat: add default param value

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,9 +1,13 @@
 import { container } from './index.styles';
 import { ButtonPropsType } from './index.types';
 
-export default function Button({ children, ...rest }: ButtonPropsType) {
+export default function Button({
+  children,
+  type = 'button',
+  ...rest
+}: ButtonPropsType) {
   return (
-    <button css={container} {...rest}>
+    <button css={container} type={type} {...rest}>
       {children}
     </button>
   );
