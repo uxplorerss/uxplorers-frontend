b458b433ee8bbfd0799b6f01a6c670d30880a73d
kim jihwan <kjhwan0802@naver.com>
2024-11-26 15:46:49 +0900

    feat: 가는 날/오는 날 버스 선택 화면 구성

--- src/routes/booking/tickets/index.tsx
+++ src/routes/booking/tickets/index.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+import { Card, TopBar, Typography } from '../../../common/components';
+import { css } from '@emotion/react';
+import LeftArrowIcon from '../../../assets/LeftArrowIcon.svg';
+import FavIcon from '../../../assets/favoriteStarIcon.svg';
 
 export const Route = createFileRoute('/booking/tickets/')({
   component: RouteComponent,
@@ -7,5 +10,35 @@ export const Route = createFileRoute('/booking/tickets/')({
 
 function RouteComponent() {
   // 가는 날(가는 길 버스를 선택하세요) 및 오는 날 페이지 구현하기
-  return 'Hello /booking/tickets/!';
+  //const selectTime = new Date(`AM 9:00`);
+
+  return (
+    <>
+      <TopBar
+        exitButton={
+          <Link onClick={() => history.go(-1)}>
+            <img src={LeftArrowIcon} />
+          </Link>
+        }
+        centerSlot={
+          <div>
+            <div>동서울 → 부산해운대</div>
+            <div>11월 27일 (수)</div>
+          </div>
+        }
+        rightSlot={<img src={FavIcon} />}
+      />
+      <Typography variant="title1" as="p" cx={{ textAlign: 'center' }}>
+        버스를 선택하세요
+      </Typography>
+
+      <div css={css({ display: 'flex', justifyContent: 'space-between' })}>
+        <button>시간: 오전 9:00 이후</button>
+        <button>검색조건</button>
+      </div>
+
+      <Card></Card>
+      <Card></Card>
+    </>
+  );
 }
