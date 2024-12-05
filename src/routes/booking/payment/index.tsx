89535c99c2d0b21bafd7505273f6c6d1279c5b96
Byeolchan Kim <qufstar46@gmail.com>
2024-12-05 12:49:02 +0900

    feat: BookingPayment page

--- src/routes/booking/payment/index.tsx
+++ src/routes/booking/payment/index.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
+import { Typography } from '../../../common/components';
+import Flex from '../../../common/components/Flex';
+import useTicketListStore from '../../../stores/useTicketListStore';
+import PrimaryCard from '../../../common/components/PrimaryCard';
+import RouteRow from '../../../common/components/RouteRow';
+import SeatDetailsTable from '../../../common/components/SeatDetailsTable';
+import FeeSumRow from '../../../common/components/FeeSumRow';
+import BookingPaymentPage from '../../../pages/BookingPaymentPage';
 
 export const Route = createFileRoute('/booking/payment/')({
   component: IndexComponent,
 });
 
 function IndexComponent() {
-  // TODO 결제하기 페이지 구현하기
-
-  return 'Hello /booking/payment/!';
+  return <BookingPaymentPage />;
 }
