9540ac244fd2a4e6c71e3f0eb1833ba4a01f7228
Byeolchan Kim <qufstar46@gmail.com>
2024-12-05 13:01:24 +0900

    fix: make cx prop optional

--- src/common/components/CompanyRow/index.types.ts
+++ src/common/components/CompanyRow/index.types.ts
@@ -3,5 +3,5 @@ import { Interpolation, Theme } from '@emotion/react';
 export interface CompanyRowPropsType {
   destId: string[];
   company: string;
-  cx: Interpolation<Theme>;
+  cx?: Interpolation<Theme>;
 }
