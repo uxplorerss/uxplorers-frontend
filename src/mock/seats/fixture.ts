879466ec3b9084cdea98117542a88b8fa18cd724
Byeolchan Kim <qufstar46@gmail.com>
2024-12-05 12:46:30 +0900

    test: reusable fixture(test data)

---
+++ src/mock/seats/fixture.ts
@@ -0,0 +1,8 @@
+import { Seat } from '../../types';
+
+export const seats: Seat[] = [
+  { type: 'adults', fee: 15200, seatNum: 12 },
+  { type: 'adults', fee: 15200, seatNum: 13 },
+  { type: 'children', fee: 15000, seatNum: 14 },
+  { type: 'children', fee: 15000, seatNum: 15 },
+];
--- src/stories/SeatDetailsTable.stories.tsx
+++ src/stories/SeatDetailsTable.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import SeatDetailsTable from '../common/components/SeatDetailsTable';
+import { ticket } from '../mock/ticket/fixture';
 
 const meta = {
   title: 'common/SeatDetailsTable',
@@ -11,13 +12,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    company: '중앙고속',
-    seats: [
-      { type: 'adults', fee: 15200, seatNum: 12 },
-      { type: 'adults', fee: 15200, seatNum: 13 },
-      { type: 'children', fee: 15000, seatNum: 14 },
-      { type: 'children', fee: 15000, seatNum: 15 },
-    ],
-    destId: [],
+    ...ticket,
   },
 };
