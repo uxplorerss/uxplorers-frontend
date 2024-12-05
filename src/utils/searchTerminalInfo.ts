91f5fd569cadc69e9f18c12076c8c74bf3b503e6
kim jihwan <kjhwan0802@naver.com>
2024-12-05 17:06:37 +0900

    feat: searchTerminal id에는 NAEK 붙이지 않는 걸로 통일

--- src/utils/searchTerminalInfo.ts
+++ src/utils/searchTerminalInfo.ts
@@ -17,10 +17,17 @@ interface ResponseGosokTerminalInfo {
   };
 }
 
+/**
+ * 터미널 코드를 받아 ㅌ터미널 이름을 반환하는 함수
+ * @param code 터미널 코드 (NAEK를 붙인 코드 | 숫자 코드)
+ * @returns 터미널 이름
+ */
 export function searchTerminalNameToCode(code: string): string | null {
   const terminal = (
     TerminalInfo as ResponseGosokTerminalInfo
-  ).response.body.items.item.find((item) => item.terminalId === code);
+  ).response.body.items.item.find(
+    (item) => item.terminalId === parseBusTerminalId(code)
+  );
 
   if (!terminal) {
     return null;
@@ -28,6 +35,11 @@ export function searchTerminalNameToCode(code: string): string | null {
   return terminal.terminalNm;
 }
 
+/**
+ * 터미널 이름을 받아 터미널 코드를 반환하는 함수
+ * @param name 터미널 이름
+ * @returns NAEK가 없는 터미널 코드
+ */
 export function searchTerminalCodeToName(name: string): string | null {
   const terminal = (
     TerminalInfo as ResponseGosokTerminalInfo
@@ -36,5 +48,17 @@ export function searchTerminalCodeToName(name: string): string | null {
   if (!terminal) {
     return null;
   }
-  return terminal.terminalId;
+  return terminal.terminalId.slice(4);
 }
+
+/**
+ * NAEK가 붙었는지 판단하는 함수
+ * @param busTmnId
+ * @returns
+ */
+export const parseBusTerminalId = (busTmnId: string) => {
+  if (busTmnId.length === 3 || Number.isNaN(Number(busTmnId)) === false) {
+    return `NAEK${busTmnId}`;
+  }
+  return busTmnId;
+};
