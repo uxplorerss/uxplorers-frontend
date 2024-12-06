58845edbde16da791e68173b871639409ae84642
kim jihwan <kjhwan0802@naver.com>
2024-12-06 21:10:43 +0900

    refactor: remove unused imports

--- src/routes/booking/payment/index.tsx
+++ src/routes/booking/payment/index.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
-import { Typography } from '../../../common/components';
-import Flex from '../../../common/components/Flex';
-import useTicketListStore from '../../../stores/useTicketListStore';
-import PrimaryCard from '../../../common/components/PrimaryCard';
-import RouteRow from '../../../common/components/RouteRow';
-import SeatDetailsTable from '../../../common/components/SeatDetailsTable';
-import FeeSumRow from '../../../common/components/FeeSumRow';
 import BookingPaymentPage from '../../../pages/BookingPaymentPage';
 
 export const Route = createFileRoute('/booking/payment/')({
