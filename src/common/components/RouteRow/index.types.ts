a83585d117f24f895b7c006cbdb0fe0732c70d2f
Byeolchan Kim <qufstar46@gmail.com>
2024-12-08 21:39:52 +0900

    feat: allow for outlined option

--- src/common/components/RouteRow/index.types.ts
+++ src/common/components/RouteRow/index.types.ts
@@ -1,5 +1,9 @@
+import { Interpolation, Theme } from '@emotion/react';
+
 export interface RouteOptionRowPropsType {
   startName: string;
   destName: string;
   inactive?: boolean;
+  outlined?: boolean;
+  cx?: Interpolation<Theme>;
 }
